0f4659ac78bb92aaa61f622d29c3dcd1648e9b5d
Carlos Esparza <haseck12@gmail.com>
2019-07-06 00:18:18 -0500

    Configuración inicial

---
+++ gulpfile.babel.js
@@ -0,0 +1,17 @@
+import { dest, series, src, watch } from 'gulp'
+import gif from 'gulp-if'
+import gsm from 'gulp-sourcemaps'
+
+const sourcemaps = false,
+
+  sass = () => {
+    return src('./scss/*.scss')
+      .pipe(gif(sourcemaps, gsm.init()))
+      .pipe(require('gulp-sass')({ includePaths: [require('node-normalize-scss').includePaths] }))
+      .pipe(gif(sourcemaps, gsm.write()))
+      .pipe(dest('./css/'))
+  }
+
+exports.default = () => {
+  watch('./scss/**/*.scss', series(sass))
+}
