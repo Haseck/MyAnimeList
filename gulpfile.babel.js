7c153172cfa9384bdd1c785d152388523e49c602
Carlos Esparza <haseck12@gmail.com>
2019-07-09 15:46:19 -0500

    Dependencias agregadas y otros cambios gulp-cssnano y gulp-imagemin, así como sus respectivas configuraciones

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,17 +1,40 @@
 import { dest, series, src, watch } from 'gulp'
 import gif from 'gulp-if'
+import imgmin from 'gulp-imagemin'
 import gsm from 'gulp-sourcemaps'
 
-const sourcemaps = false,
-
+const sourcemaps = true,
+  paths = {
+    scss: {
+      src: './src/scss/*.scss',
+      dest: './dist/css',
+      wtc: './src/scss/**/*.scss'
+    },
+    img: {
+      src: './src/image/**/*.png',
+      dest: './dist/image'
+    }
+  },
   sass = () => {
-    return src('./scss/*.scss')
+    return src(paths.scss.src)
       .pipe(gif(sourcemaps, gsm.init()))
       .pipe(require('gulp-sass')({ includePaths: [require('node-normalize-scss').includePaths] }))
+      .pipe(require('gulp-cssnano')({
+        autoprefixer: {
+          add: true,
+          browsers: '> 1%, last 2 versions, Firefox ESR, Opera 12.1'
+        }
+      }))
       .pipe(gif(sourcemaps, gsm.write()))
-      .pipe(dest('./css/'))
+      .pipe(dest(paths.scss.dest))
+  },
+  img = () => {
+    return src(paths.img.src)
+      .pipe(imgmin([imgmin.gifsicle(), imgmin.jpegtran(), imgmin.optipng(), imgmin.svgo()], { verbose: true }))
+      .pipe(dest(paths.img.dest))
   }
 
 exports.default = () => {
-  watch('./scss/**/*.scss', series(sass))
+  watch(paths.scss.wtc, series(sass))
+  watch(paths.img.src, series(img))
 }
